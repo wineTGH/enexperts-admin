022ae72bd7f5572941ad7914ba2da698ec1d2192
winet <tolya.bogomolov2019@gmail.com>
2024-03-17 23:17:09 +1000

    debug page for user

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    export let data;
+</script>
+
+<section class="w-full gap-2 grid place-items-center">
+    <h1 class="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+        Welcome {data.user?.firstName || ""} to the "enexperts"<br />
+        dashboard page
+    </h1>
+    <p class="max-w-[750px] text-center text-lg sm:text-xl text-balance text-muted-foreground">Please Log In or make new account</p>
+</section>
