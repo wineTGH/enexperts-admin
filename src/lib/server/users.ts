e2b3b2789b21426b698576e47445404227312d7a
winet <tolya.bogomolov2019@gmail.com>
2024-03-28 22:54:58 +1000

    methods to get users without sensitive info

--- src/lib/server/users.ts
+++ src/lib/server/users.ts
@@ -3,7 +3,6 @@ import { db } from "$lib/server/db/index.js";
 import { usersTable } from "$lib/server/db/schema.js";
 import type { NewUser, User } from "./db/types";
 
-
 export async function isUserExists(username: string = "", email: string = "") {
     const result = await db.selectDistinct().from(usersTable).where(
         or(
@@ -21,4 +20,31 @@ export async function getUser(username: string) {
 
 export async function createNewUser(user: NewUser): Promise<User> {
     return (await db.insert(usersTable).values(user).returning())[0];
+}
+
+export async function getSavedUsers(page: number, limit: number = 15): Promise<import("lucia").User[]> {
+    return await db.select({
+        id: usersTable.id,
+        username: usersTable.username,
+        email: usersTable.email,
+        firstName: usersTable.firstName,
+        lastName: usersTable.lastName,
+        role: usersTable.role
+    })
+    .from(usersTable)
+    .limit(limit)
+    .offset(limit * page);
+}
+
+export async function getSavedUser(id: string): Promise<import("lucia").User | null> {
+    const user = (await db.select({
+        id: usersTable.id,
+        username: usersTable.username,
+        email: usersTable.email,
+        firstName: usersTable.firstName,
+        lastName: usersTable.lastName,
+        role: usersTable.role
+    }).from(usersTable).where(eq(usersTable.id, id)))[0];
+
+    return user || null;
 }
