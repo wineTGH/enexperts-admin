14c72b22401c6d82940f3de60e5ea054d4fac619
winet <tolya.bogomolov2019@gmail.com>
2024-03-28 22:59:10 +1000

    user edit schemas

---
+++ src/routes/(protected)/dashboard/users/[id]/schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const editUserSchema = z.object({
+    username: z.string().min(2).max(50).regex(/^[a-z0-9_]+$/),
+    email: z.string().email(),
+    
+    firstName: z.string().min(2).max(50),
+    lastName: z.string().min(2).max(50),
+})
+
+export const changePasswordSchema = z.object({
+    password: z.string().min(8).max(64),
+    repeatPassword: z.string().min(8).max(64),
+}).superRefine(({password, repeatPassword}, ctx) => {
+    if (password !== repeatPassword) {
+        ctx.addIssue({
+            code: "custom",
+            path: ["repeatPassword"],
+            message: "Passwords don't match"
+        })
+    }
+})
+
+
+export type EditUserSchema = typeof editUserSchema;
+export type ChangePasswordSchema = typeof changePasswordSchema;
