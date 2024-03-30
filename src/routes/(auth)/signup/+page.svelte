fbf616511cec4dffa42501e8956a22a17f1c0275
winet <tolya.bogomolov2019@gmail.com>
2024-03-30 21:53:44 +1000

    Fixed sign up layout

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -14,55 +14,64 @@
  
   const { form: formData, enhance } = form;
 </script>
-
-<form method="POST" use:enhance class="w-full p-6 sm:w-auto sm:p-0">
-    <div class="flex flex-col sm:gap-3 sm:flex-row">
-        <Form.Field {form} name="username">
-            <Form.Control let:attrs>
-                <Form.Label>Username</Form.Label>
-                <Input placeholder="Username" {...attrs} bind:value={$formData.username} required />
-            </Form.Control>
-            <Form.FieldErrors />
-        </Form.Field>
-        <Form.Field {form} name="email">
-            <Form.Control let:attrs>
-                <Form.Label>Email</Form.Label>
-                <Input placeholder="Email" type="email" {...attrs} bind:value={$formData.email} required />
-            </Form.Control>
-            <Form.FieldErrors />
-        </Form.Field>
-    </div>
-
-    <div class="flex flex-col sm:gap-3 sm:flex-row">
-        <Form.Field {form} name="firstName">
-            <Form.Control let:attrs>
-                <Form.Label>First name</Form.Label>
-                <Input placeholder="Your name" type="text" {...attrs} bind:value={$formData.firstName} required />
-            </Form.Control>
-            <Form.FieldErrors />
-        </Form.Field>
-        <Form.Field {form} name="lastName">
-            <Form.Control let:attrs>
-                <Form.Label>Last Name</Form.Label>
-                <Input placeholder="Last Name" type="text" {...attrs} bind:value={$formData.lastName} required />
-            </Form.Control>
-            <Form.FieldErrors />
-        </Form.Field>
-    </div>
-
-    <Form.Field {form} name="password">
-        <Form.Control let:attrs>
-            <Form.Label>Password</Form.Label>
-            <Input placeholder="Password" type="password" {...attrs} bind:value={$formData.password} required />
-        </Form.Control>
-        <Form.FieldErrors />
-    </Form.Field>
-
-    <Form.Field {form} name="repeatPassword">
-        <Form.Control let:attrs>
-            <Input placeholder="Repeat password" type="password" {...attrs} bind:value={$formData.repeatPassword} required />
-        </Form.Control>
-        <Form.FieldErrors />
-    </Form.Field>
-    <Form.Button>Register</Form.Button>
-</form>
+<main class="container flex flex-col gap-2 sm:gap-6 items-center justify-center h-screen">
+    <h1 class="font-bold text-3xl sm:text-4xl">Welcome, newcomer!</h1>
+    <form method="POST" use:enhance class="p-6 flex flex-col items-center sm:w-auto gap-2 sm:p-0">
+        <div class="flex flex-col sm:gap-6 sm:flex-row">
+            <Form.Field {form} name="username">
+                <Form.Control let:attrs>
+                    <Form.Label>Username</Form.Label>
+                    <Input placeholder="john_doe" {...attrs} bind:value={$formData.username} required />
+                </Form.Control>
+                <Form.FieldErrors />
+            </Form.Field>
+            <Form.Field {form} name="email">
+                <Form.Control let:attrs>
+                    <Form.Label>Email</Form.Label>
+                    <Input placeholder="me@example.com" type="email" {...attrs} bind:value={$formData.email} required />
+                </Form.Control>
+                <Form.FieldErrors />
+            </Form.Field>
+        </div>
+    
+        <div class="flex flex-col sm:gap-6 sm:flex-row">
+            <Form.Field {form} name="firstName">
+                <Form.Control let:attrs>
+                    <Form.Label>First name</Form.Label>
+                    <Input placeholder="John" type="text" {...attrs} bind:value={$formData.firstName} required />
+                </Form.Control>
+                <Form.FieldErrors />
+            </Form.Field>
+            <Form.Field {form} name="lastName">
+                <Form.Control let:attrs>
+                    <Form.Label>Last Name</Form.Label>
+                    <Input placeholder="Doe" type="text" {...attrs} bind:value={$formData.lastName} required />
+                </Form.Control>
+                <Form.FieldErrors />
+            </Form.Field>
+        </div>
+    
+        <div class="flex flex-col sm:gap-6 sm:flex-row">
+            <Form.Field {form} name="password">
+                <Form.Control let:attrs>
+                    <Form.Label>Password</Form.Label>
+                    <Input placeholder="********" type="password" {...attrs} bind:value={$formData.password} required />
+                </Form.Control>
+                <Form.FieldErrors />
+            </Form.Field>
+        
+            <Form.Field {form} name="repeatPassword">
+                <Form.Control let:attrs>
+                    <Form.Label>Repeat Password</Form.Label>
+                    <Input placeholder="********" type="password" {...attrs} bind:value={$formData.repeatPassword} required />
+                </Form.Control>
+                <Form.FieldErrors />
+            </Form.Field>
+        </div>
+        <Form.Button>Register</Form.Button>
+    </form>
+    <footer class="flex gap-2 justify-center items-baseline">
+        <a class="underline text-foreground hover:text-foreground/80" href="/signin">Sign In</a>
+        <a class="underline text-foreground hover:text-foreground/80" href="/legal">Legal</a>
+    </footer>
+</main>
