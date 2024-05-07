e30fa7565642a82d58b47157da20255ec93f2d4c
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-05-07 11:02:21 +1000

    User editing

--- src/routes/(protected)/dashboard/users/[id]/+page.server.ts
+++ src/routes/(protected)/dashboard/users/[id]/+page.server.ts
@@ -2,30 +2,14 @@ import { getSavedUser, isUserExists } from "$lib/server/users";
 import { error, fail } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
 
-import { superValidate } from "sveltekit-superforms";
+import { message, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { editUserSchema } from "./schema";
+import { db } from "$lib/server/db";
+import { usersTable } from "$lib/server/db/schema";
+import { and, eq, ne, or } from "drizzle-orm";
+import { lucia } from "$lib/server/auth";
 
-const serverEditUserSchema = editUserSchema.superRefine(async ({username, email, id}, ctx) => {
-    const usernameResult = await isUserExists(username);
-    const emailResult = await isUserExists(email);
-
-    if (emailResult.user?.email === email && emailResult.user?.id !== id) {
-        ctx.addIssue({
-            path: ["email"],
-            code: "custom",
-            message: "Email already exists"
-        });
-    }
-
-    if (usernameResult.user?.username === username && usernameResult.user?.id !== id) {
-        ctx.addIssue({
-            path: ["username"],
-            code: "custom",
-            message: "Username already exists"
-        });
-    }
-})
 
 export const load: PageServerLoad = async ({ params }) => {
     const editUser = await getSavedUser(params.id);
@@ -35,8 +19,36 @@ export const load: PageServerLoad = async ({ params }) => {
 
 export const actions: Actions = {
     editUser: async (event) => {
-        const form = await superValidate(event, zod(serverEditUserSchema));
+        const form = await superValidate(event, zod(editUserSchema));
+
+        if (form.data.id !== event.params.id && event.locals.session?.id) {
+            await lucia.invalidateSession(event.locals.session.id);
+    
+            const sessionCookie = lucia.createBlankSessionCookie();
+            event.cookies.set(sessionCookie.name, sessionCookie.value, {
+                path: '.',
+                ...sessionCookie.attributes
+            });
+    
+            return message(form, "What are you trying to do? 🤨📸", {status: 418});
+        }
+
+        const existingUsers = await db.select().from(usersTable).where(
+            and(
+                ne(usersTable.id, event.params.id),
+                or(eq(usersTable.username, form.data.username), eq(usersTable.email, form.data.email))
+            )
+        );
+
+        existingUsers.forEach((user) => {
+            if (user.username === form.data.username) { form.errors.username = ["Username already exists"]; }
+            if (user.email === form.data.email) { form.errors.email = ["Email already exists"]; }
+        });
 
         if (!form.valid) { return fail(400, { form }); }
+
+        await db.update(usersTable).set(form.data).where(eq(usersTable.id, event.params.id));
+
+        return message(form, "User updated");
     }
 }
--- src/routes/(protected)/dashboard/users/[id]/+page.svelte
+++ src/routes/(protected)/dashboard/users/[id]/+page.svelte
@@ -26,56 +26,56 @@
 
 <div class="container mx-auto py-10">
     <h1 class="text-3xl font-bold">Edit {editUser?.username}</h1>
+    <form method="POST" action="?/editUser" use:enhance>
+        <Form.Field {form} name="id">
+            <Form.Control let:attrs>
+                <Input type="hidden" {...attrs} bind:value={$formData.id} required />
+            </Form.Control>    
+        </Form.Field>
+        <Form.Field {form} name="username">
+            <Form.Control let:attrs>
+                <Form.Label>Username</Form.Label>
+                <Input placeholder="john_doe" type="text" {...attrs} bind:value={$formData.username} required />
+            </Form.Control>    
+        </Form.Field>
+        <Form.Field {form} name="email">
+            <Form.Control let:attrs>
+                <Form.Label>Email</Form.Label>
+                <Input placeholder="me@me.com" type="email" {...attrs} bind:value={$formData.email} required />
+            </Form.Control>
+        </Form.Field>
+    
+        <Form.Field {form} name="firstName">
+            <Form.Control let:attrs>
+                <Form.Label>First Name</Form.Label>
+                <Input placeholder="John" type="text" {...attrs} bind:value={$formData.firstName} required />
+            </Form.Control>
+        </Form.Field>
+    
+        <Form.Field {form} name="lastName">
+            <Form.Control let:attrs>
+                <Form.Label>Last Name</Form.Label>
+                <Input placeholder="Doe" type="text" {...attrs} bind:value={$formData.lastName} required />
+            </Form.Control>
+        </Form.Field>
+    
+        <Form.Field {form} name="role">
+            <Form.Control let:attrs>
+                <Form.Label>Role</Form.Label>
+                <Select.Root >
+                    <Select.Trigger class="w-44" {...attrs}>
+                        <Select.Value placeholder={$formData.role}>{$formData.role}</Select.Value>
+                    </Select.Trigger>
+                    <Select.Content>
+                        {#each roles as role}
+                            <Select.Item value={role}>{role}</Select.Item>
+                        {/each}
+                    </Select.Content>
+                </Select.Root>
+            </Form.Control>
+        </Form.Field>
+    
+        <Form.Button>Save</Form.Button>
+    </form>
 </div>
 
-<form method="POST" action="?/editUser" use:enhance>
-    <Form.Field {form} name="id">
-        <Form.Control let:attrs>
-            <Input type="hidden" {...attrs} bind:value={$formData.id} required />
-        </Form.Control>    
-    </Form.Field>
-    <Form.Field {form} name="username">
-        <Form.Control let:attrs>
-            <Form.Label>Username</Form.Label>
-            <Input placeholder="john_doe" type="text" {...attrs} bind:value={$formData.username} required />
-        </Form.Control>    
-    </Form.Field>
-    <Form.Field {form} name="email">
-        <Form.Control let:attrs>
-            <Form.Label>Email</Form.Label>
-            <Input placeholder="me@me.com" type="email" bind:value={$formData.email} required />
-        </Form.Control>
-    </Form.Field>
-
-    <Form.Field {form} name="firstName">
-        <Form.Control let:attrs>
-            <Form.Label>First Name</Form.Label>
-            <Input placeholder="John" type="text" bind:value={$formData.firstName} required />
-        </Form.Control>
-    </Form.Field>
-
-    <Form.Field {form} name="lastName">
-        <Form.Control let:attrs>
-            <Form.Label>Last Name</Form.Label>
-            <Input placeholder="Doe" type="text" bind:value={$formData.lastName} required />
-        </Form.Control>
-    </Form.Field>
-
-    <Form.Field {form} name="role">
-        <Form.Control let:attrs>
-            <Form.Label>Role</Form.Label>
-            <Select.Root>
-                <Select.Trigger class="w-44">
-                    <Select.Value placeholder={$formData.role}>{$formData.role}</Select.Value>
-                </Select.Trigger>
-                <Select.Content>
-                    {#each roles as role}
-                        <Select.Item value={role}>{role}</Select.Item>
-                    {/each}
-                </Select.Content>
-            </Select.Root>
-        </Form.Control>
-    </Form.Field>
-
-    <Form.Button>Save</Form.Button>
-</form>
