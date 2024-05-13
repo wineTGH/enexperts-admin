ef1cc01da4743b3e0f8fa9ba9b39a111ef294ecc
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-05-13 21:56:46 +1000

    New first page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
+    import Button from '$lib/components/ui/button/button.svelte';
+    import SampleScreenshot from "$lib/assets/images/sample-screenshot.png?enhanced";
+
     export let data;
 </script>
 
-<section class="w-full gap-2 grid place-items-center">
-    <h1 class="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-        Welcome {data.user?.firstName || ""} to the "enexperts"<br />
-        dashboard page
-    </h1>
-    {#if !data.user}
-        <p class="max-w-[750px] text-center text-lg sm:text-xl text-balance text-muted-foreground">Please Log In or make new account</p>
-    {/if}
+<section class="w-full grid grid-cols-2 gap-6 items-center justify-between">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-3xl font-bold leading-tight tracking-tighter md:text-5xl">
+            Welcome{data.user?.firstName? " back" : ""} to <span class="underline text-primary">enxeperts</span>!
+        </h1>
+        <p class="text-muted-foreground text-pretty max-w-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Nihil dolor cupiditate quia pariatur ad doloribus nesciunt enim quae, 
+            quam tenetur explicabo in placeat quisquam natus obcaecati. 
+            Sunt veniam omnis voluptates?
+        </p>
+        {#if !data.user}
+            <div class="flex gap-4 items-center text-center">
+                <a href="/signin" class="transition-colors hover:text-foreground/80 text-foreground">Sign In</a>
+                <Button href="/signup">Sign Up</Button>
+            </div>
+        {:else}
+            <Button href="/dashboard" class="w-fit">To Dashboard</Button>
+        {/if}
+    </div>
+    <div class="h-full">
+        <enhanced:img src={SampleScreenshot} alt="Screenshot of user's page from dashboard" />
+        <small class="italic text-muted-foreground">
+            Example of a dashboard interface. Should be changed when a better design is available.
+        </small>
+    </div>
 </section>
